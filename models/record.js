880254e055711ec1c72cd43b8acdfd42a70c1cef
ClementPan <zzz2999582@gmail.com>
2020-08-04 14:32:50 +0800

    feat: arrange basic view

--- models/record.js
+++ models/record.js
@@ -2,7 +2,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const RecordSchema = new Schema({
-
+  name: {
+    type: String,
+    required: true
+  },{
+  date: {
+    type: String,
+    required: true
+  },{
+  category: {
+    type: String,
+    required: true
+  },{
+  amount: {
+    type: String,
+    required: true
+  }
 })
 
 
